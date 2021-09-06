b882e348e9987a2c59ec9b4febc29cea9fc9acf8
Sergii Griazin <gig_troy@ukr.net>
2021-09-06 16:48:50 +0300

    task-03

---
+++ js/task-03.js
@@ -0,0 +1,40 @@
+const images = [
+{
+url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+alt: 'White and Black Long Fur Cat',
+},
+{
+url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+},
+{
+url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+alt: 'Group of Horses Running',
+},
+];
+
+const galleryList = document.getElementById(`gallery`)
+console.dir(galleryList);
+
+images.forEach(element => {
+    galleryList.insertAdjacentHTML(
+        "afterbegin",  `<li><img src = "${element.url}" alt = "${element.alt}"  width = "300" height = "200" /></li>`,
+    )
+});
+galleryList.style.listStyle = `none`
+galleryList.style.display = `flex`
+galleryList.style.marginTop = `100px`
+galleryList.style.justifyContent = `space-between`
+
+
+// Задание 3
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li. 
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
